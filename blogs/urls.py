100a22acc88b9ee6012ff7b5c6c4a319b95d53f8
Shreshtha731 <kumarshreshtha731@gmail.com>
2026-09-13 15:58:42 +0000

    fix url routing in blogs

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
-   path("",views.home_page,name="home"),
-   path("allposts/",views.blogposts,name="all-posts"),   #app level basically the second leveldd  allpostss section
-   path("allposts/<slug:blog>/",views.blog_post ,name="blog-post") #this syntax tells django that to any url after slash for allposts  under allposts section  name is used to name our url and name can be anything now in the above syntax instead of harcoding allposts we can use blog-post
-  
+urlpatterns = [
+    path("", views.home_page, name="home"),
+    path("allposts/", views.blogposts, name="all-posts"),
+    path("allposts/<slug:blog>/", views.blog_post, name="blog-post"),
 ]
